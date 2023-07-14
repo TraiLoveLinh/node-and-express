348b18e0b9e813a50c96cdd6b7114d3889475389
heo16072007 <traipupgmobile@gmail.com>
2023-07-14 08:58:59 +0700

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -2,12 +2,12 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
-function serve_string(req, res){
-    res.send("Hello Express");
-}
+//function serve_string(req, res){
+//    res.send("Hello Express");
+//}
 
 //app.get("",serve_string);
-absolutePath = __dirname + 'views/index.html';
+let absolutePath = __dirname + '/views/index.html';
 
 app.get("", absolutePath);
 
