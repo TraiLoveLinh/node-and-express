345b78140e75ca167a904ec2090e01986bfa54a0
heo16072007 <traipupgmobile@gmail.com>
2023-07-14 08:46:53 +0700

    modified:   myApp.js 	modified:   server.js

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+function serve_string(req, res){
+    res.send("Hello Express");
+}
+
+app.get("",serve_string);
 
 
 
