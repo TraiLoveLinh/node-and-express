995eae64e22af32819575230f1d60c0c8c484132
heo16072007 <traipupgmobile@gmail.com>
2023-07-14 09:38:17 +0700

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -6,14 +6,14 @@ console.log("Hello World");
 //    res.send("Hello Express");
 //}
 //app.get("",serve_string);
-/* 
+ 
 let indexIhtml_absolutePath = __dirname + '/views/index.html'; 
 function serve_file(req, res){
     res.sendFile(indexIhtml_absolutePath);
 }
 app.get("", serve_file);
 
-*/
+
 app.use("/public", express.static(__dirname + '/public'));
 
 
