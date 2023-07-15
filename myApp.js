135f240b0464324caaa18794e118a211a69c193e
heo16072007 <traipupgmobile@gmail.com>
2023-07-15 15:09:31 +0700

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -57,6 +57,12 @@ app.get('/:word/echo', function(req, res, next){
     }
     res.json(data);
 });
+app.get('/name?first=firstname&last=lastname', function(req, res){
+    const data = {
+        name: req.query
+    }
+    res.json(data);
+});
 
 
 
